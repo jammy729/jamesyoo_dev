daaafdf145f75e2847f9faeb4a4aae72a2a7774b
James Yoo <jyoo3607@gmail.com>
2023-04-11 14:51:38 -0700

    update

---
+++ client/src/pages/work/institution-database.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const institution-database = () => {
+  return (
+    <div>institution-database</div>
+  )
+}
+
+export default institution-database
---
+++ client/src/pages/work/visual-analytics.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const visual-analytics = () => {
+  return (
+    <div>visual-analytics</div>
+  )
+}
+
+export default visual-analytics
